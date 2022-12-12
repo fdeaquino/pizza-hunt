5477fe3e35a0ce4757a5e5fe0dc99a93e2b06b8b
FDeaquino <fidel.deaquino@gmail.com>
2022-12-12 11:24:54 -0600

    created the Comment model, associated the Pizza and Comment models, created the Comment controller methods, created and tested the routes, Integrated with the front end and refactored

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
+
+
+// exports from    `comment-routes.js`
+router.use('/comments', commentRoutes);
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
